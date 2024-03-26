58c83ed5455f89c02f0398907f671acb4ab42a1f
nkarthiq <n.karthiq@gmail.com>
2024-03-26 20:55:11 +0530

    task 2 complete

--- dom-day1/script.js
+++ dom-day1/script.js
@@ -1,15 +1,8 @@
-let div1=document.createElement("div");
-div1.innerHTML="<p>this is div</p>";
-document.body.append(div1);
-let div2=document.createElement("div");
-div2.innerText="<p>this is div</p>";
-document.body.append(div2);
-div1.setAttribute("class","main");
-div1.setAttribute("id","div1");
-div1.append(div2);
+
 
 let box=document.createElement("div");
 box.className="box";
+document.body.append(box);
 
 let cont=document.createElement("div");
 cont.className="container";
@@ -17,24 +10,33 @@ box.append(cont);
 let row1=document.createElement("div");
 row1.className="row";
 cont.append(row1);
+
 let col1=document.createElement("div");
 col1.className="col-md-4";
 row1.append(col1);
 
 let boxpart=document.createElement("div");
 boxpart.className="box-part text-center";
-col1.append(col1);
+col1.append(boxpart);
 
-<div class="box">
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="box-part text-center">
-                <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-                <h4>Instagram</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    culpa accusantium recusandae doloremque iusto
-                </p>
-                <a href="#" target="_blank">Learn More</a>
-            </div>
-        </div>
+let instaicon=document.createElement("i");
+instaicon.className="fa fa-instagram fa-3x";
+instaicon.setAttribute("aria-hidden","true")
+boxpart.append(instaicon);
+
+let instahead=document.createElement("h4");
+instahead.innerText="Instagram";
+boxpart.append(instahead);
+
+let para=document.createElement("p");
+para.innerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. culpa accusantium recusandae doloremque iusto";
+boxpart.append(para);
+
+let link=document.createElement("a");
+link.innerText="Learn More";
+link.href="#";
+link.target="_blank";
+boxpart.append(link);
+
+
+
